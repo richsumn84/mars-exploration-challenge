be95fde8b0ad49b5ba23713400c7d16bad2381e2
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-07-30 21:26:12 -0300

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('String', () => {
+	describe('hello()', () => {
+		it('should return Hello Xerpa! when the function is called', () => {
+			assert.equal('Hello Xerpa!', 'Hello Xerpa!');
+		});
+	});
+});
