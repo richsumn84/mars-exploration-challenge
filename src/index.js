3ee8c23b712c94f1107a0dcd9ead35c1bf0149d5
Henrique Carvalho da Cruz <henriquecarv.cruz@gmail.com>
2018-08-01 23:00:47 -0300

    Split line instructions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 'use strict';
 const io = require('./util/IO');
 
-io.readFile();
+(async () => {
+	try {
+		const commands = await io.readFile();
 
-// TODO Application
+		const upperRightCorner = commands[0];
+
+		let devices = [];
+
+		for (let index = 1; index < commands.length; index++) {
+			const firstEntry = commands[index];
+			const secondEntry = commands[index + 1];
+			const deviceInstruction = {
+				initialPosition: firstEntry,
+				moves: secondEntry
+			};
+			devices.push(deviceInstruction);
+			index++;
+		}
+	} catch (err) {
+		throw err;
+	}
+})();
--- src/util/IO.js
+++ src/util/IO.js
@@ -3,20 +3,27 @@ const fs = require('fs');
 const path = require('path');
 const inputFilePath = path.join(__dirname, './../doc/input.txt');
 
-class IO {
-	readFile() {
-		const buffer = new Buffer(512);
+const readFile = () => {
+	const buffer = new Buffer(512);
 
+	return new Promise((resolve, reject) => {
 		fs.open(inputFilePath, 'r', (err, fd) => {
-			if (err) return console.error(err);
+			if (err) reject(err);
 
 			fs.read(fd, buffer, 0, buffer.length, 0, (err, bytes) => {
-				if (err) return console.error(err);
+				if (err) reject(err);
 
-				if (bytes > 0) console.log(buffer.slice(0, bytes).toString());
+				if (bytes > 0)
+					resolve(
+						buffer
+							.slice(0, bytes)
+							.toString()
+							.split('\n')
+					);
+				resolve([]);
 			});
 		});
-	}
-}
+	});
+};
 
-module.exports = new IO();
+module.exports = { readFile };
